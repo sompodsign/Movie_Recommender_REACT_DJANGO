237bce1d063de53b9c2ca3eb3f11e1ea80957340
Shampad Sharkar <sompodsign@gmail.com>
2021-03-20 23:54:05 +0600

    improved the card.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import axi from "./axiosConfig";
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import {Button} from '@material-ui/core';
 import Movie from "./components/Movie"
 
 function App() {
 
-    const [movie, setMovie] = useState({})
+    const [movie, setMovie] = useState({});
 
-    async function handleClick() {
-        await axi.get("/api/v1").then(r => setMovie(r.data.movie));
+    useEffect(()=> {
+        axi.get("/api/v1").then(r => setMovie(r.data.movie));
+    }, [])
+
+    function handleClick(){
+
     }
-
-    // console.log(movie);
 
     return (
         <div>
-            {
-                <Movie />
-// Object.keys(movie).length !== 0 &&
 
-            }
-
+                <Movie movie={movie}/>
+{/*// Object.keys(movie).length !== 0 &&*/}
 
             <p>
                 <Button className="btn" variant="contained" color="secondary" onClick={handleClick}>Suggest</Button>
@@ -30,19 +29,3 @@ function App() {
 }
 
 export default App;
-
-
-//
-// <div className="flip-card">
-//     // <div className="flip-card-inner">
-//     // <div className="flip-card-front">
-//     // <img style={{width: 400, height: 500}} src={movie.poster} alt="poster"/>
-//     // </div>
-//     // <div className="flip-card-back">
-//     // <List movie={movie.title}/>
-//     // <List movie={movie.overview}/>
-//     // <List movie={movie.genre}/>
-//     // <List movie={movie.rating}/>
-//     // </div>
-//     // </div>
-//     // </div>
--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -2,9 +2,20 @@ import React from "react";
 
 function Movie(props){
 
-    // console.log(props)
+    console.log(props.movie.title)
 
-    return
+    return <div className="card movie_card">
+        <img src={props.movie.poster}
+             className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <i className="fas fa-play play_button" data-toggle="tooltip" data-placement="bottom"
+                   title="Play Trailer">
+                </i>
+                <h5 className="card-title">{props.movie.title}</h5>
+                <span className="movie_info">{props.movie.year}</span>
+                <span className="movie_info float-right"><i className="fas fa-star"></i> {props.movie.rating} / 10</span>
+            </div>
+    </div>
 
 }
 
