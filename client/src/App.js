81abca3e1a65a18d32ef43cb7c3c484354f29dd5
Shampad Sharkar <sompodsign@gmail.com>
2021-03-22 01:04:46 +0600

    card position fixed.

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Movie from "./components/Movie"
 function App() {
 
     const [movie, setMovie] = useState({});
+    const [isloading, setLoading] = useState(false)
 
     function fetchMovie() {
         axi.get("/api/v1").then(r => setMovie(r.data.movie));
@@ -20,7 +21,7 @@ function App() {
     }
 
     return (
-        <div>
+        <div className="outer">
             <Movie movie={movie}/>
             <p>
                 <Button className="btn" variant="contained" color="secondary" onClick={handleClick}>Next</Button>
@@ -30,3 +31,5 @@ function App() {
 }
 
 export default App;
+
+//TODO: will implement loading spinner while fetching data.
